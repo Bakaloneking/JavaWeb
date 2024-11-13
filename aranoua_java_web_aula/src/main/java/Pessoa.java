f8dd29811c0ba6d4f7f63243487ddda8575490e8
Carlos Nascimento <77477586+Bakaloneking@users.noreply.github.com>
2024-11-13 11:33:54 -0400

    Add files via upload

---
+++ aranoua_java_web_aula/src/main/java/Pessoa.java
@@ -0,0 +1,23 @@
+public class Pessoa {
+    private int id;
+    private String nome;
+    private String email;
+
+
+    public Pessoa(int id, String nome, String email) {
+        this.id = "id";
+        this.nome = "nome";
+        this.email = "email";
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public int getId(){
+
+    }
+}
