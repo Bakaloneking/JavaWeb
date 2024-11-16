3bfd34a77d7220729b795bb964c4a996ca88cf12
Carlos Nascimento <77477586+Bakaloneking@users.noreply.github.com>
2024-11-16 11:06:21 -0400

    Update TestaConexaoBDComDAO.java

--- aranoua_java_web_aula/src/main/java/TestaConexaoBDComDAO.java
+++ aranoua_java_web_aula/src/main/java/TestaConexaoBDComDAO.java
@@ -1,10 +1,25 @@
 public class TestaConexaoBDComDAO{
   psvm{
     try{
+      //inserir
     Pessoa pessoa = new Pessoa("Pessoa 08",9222222222 ,"Email08@email.com");
     PessoaDAO pessoaDAO = new PessoaDAO();
     pessoaDAO.inserir(pessoa);
     sout("Inserido com sucesso!");
+      //
+      //
+      //listar
+      List<Pessoa> pessoas = pessoaDAO.listar();
+      for(Pessoa pessoa:pessoas){
+
+      }
+      /*pessoaDAO.listar();*/
+        sout("Listado com sucesso");
+      //consultar
+      Pessoa pessoa = pessoaDAO.sonsultar(5);
+      souts
+      sout("Consultado com sucesso!");
+      
     }catch(SQLException e){
       sout("ERRO:"+e.getMessage());
     }
